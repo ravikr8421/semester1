d458f52603bbe08ec7718d56301a53e11d572dc4
ravikr8421 <116019401+ravikr8421@users.noreply.github.com>
2022-10-25 13:37:31 +0530

    program to add two numbers

---
+++ addingtwonumbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the first number you want to add");
+    scanf("%d",&a);
+    printf("enter the second number you want to add");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum of two no you entered = %d",c);
+    getch();
+}    
